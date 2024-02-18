28f8d4063b3c73c8c52f4d469112c3e4bd73edb5
DibyaRB <dibyabhoidibya@gmail.com>
2024-02-18 14:46:33 +0530

    Adding documentation

--- asyncActions.js
+++ asyncActions.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
 const createStore= redux.createStore;
 const applyMiddleWare = redux.applyMiddleware;
+
+//Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, 
+//or to dispatch only if a certain condition is met. The inner function receives the store methods dispatch and getState as parameters.
+
 const thunkMiddleware = require('redux-thunk');
 const axios = require('axios');
 const initialState = {
